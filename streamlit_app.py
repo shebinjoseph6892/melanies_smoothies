092ef95f64aac71886afc6f1712942b67403ee28
shebinjoseph6892 <116418010+shebinjoseph6892@users.noreply.github.com>
2026-06-01 20:51:11 +0530

    Clean up code by removing commented sections
    Removed commented-out code for SQL insert statement and requests, cleaned up the ingredient string construction.

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,11 +23,14 @@ if ingredients_list:
     #st.write(ingredients_list)
     #st.text(ingredients_list)
     ingredients_string = ''
+  
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-                    values ('""" + ingredients_string + """','""" + Name_on_order + """')"""
+    #my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                    #values ('""" + ingredients_string + """','""" + Name_on_order + """')"""
 
    # st.write(my_insert_stmt)
    # st.stop()
@@ -36,7 +39,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {Name_on_order}', icon="✅")
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
