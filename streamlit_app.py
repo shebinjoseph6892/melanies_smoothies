ac545f7275893707d790722ed86161017c27a721
shebinjoseph6892 <116418010+shebinjoseph6892@users.noreply.github.com>
2026-06-01 21:21:48 +0530

    Update selected columns in dataframe query

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.write('The Name on smoothie will be:', Name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select (col("SEARCH_ON"))
+my_dataframe = session.table("smoothies.public.fruit_options").select (col("FRUIT_NAME"), col("SEARCH_ON"))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 ingredients_list= st.multiselect ('Choose up to 5 ingredients:', my_dataframe )
