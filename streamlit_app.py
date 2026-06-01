bad8e435b1bcee32e6832dba5b9e34f1ce7474e1
shebinjoseph6892 <116418010+shebinjoseph6892@users.noreply.github.com>
2026-06-01 20:21:01 +0530

    Refactor requests import and API call
    Refactor import statement and API call for smoothie fruit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,4 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {Name_on_order}', icon="✅")
-      import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+import requests smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")st.text(smoothiefroot_response)
